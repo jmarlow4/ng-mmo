d61c087c7a3b52c84c4956c0634d4c8f580445c3
Jason Marlow <jasonmarlow@gmail.com>
2015-05-31 16:25:01 -0600

    added express server

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('default', function() {
+  nodemon({
+    script: 'index.js',
+    ext: 'js',
+    env: {
+      PORT: 8000
+    },
+    ignore: [
+      './node_modules/**'
+    ]
+  });
+})
--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express'),
-  app = express(),
-  server = require('http').createServer(app),
-  io = require('socket.io')(server);
-
-server.listen(8000);
