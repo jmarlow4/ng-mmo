19149d7294899023062cdf62415009431f2bef42
Jason Marlow <jasonmarlow@gmail.com>
2015-06-07 10:52:25 -0600

    added $locationProvider

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,10 @@
 var app = angular.module('rl-app', ['ngRoute']);
 
-app.config(function($routeProvider){
+app.config(function($routeProvider, $locationProvider){
   $routeProvider
     .when('/', {
       templateUrl: 'js/game/gameView.html',
       controller: 'gameCtrl'
     })
+  $locationProvider.html5Mode(true);
 });
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('./app');
 //var debug = require('debug')('roguelands:server');
 var http = require('http');
 
-var port = process.env.PORT || '8000';
+var port = process.env.PORT || 8000;
 app.set('port', port);
 
 var server = http.createServer(app);
